0ea22e3861fb3e60a82f8154da07330e1b121b17
hossein-heydari <hosseinheydari790228@gmail.com>
2021-06-15 20:46:30 +0430

    done

--- assets/ts/main.ts
+++ assets/ts/main.ts
@@ -13,12 +13,12 @@ form.addEventListener("submit", (e): void => {
 });
 
 function addDo(goal: string, date: string): void {
-  let tr: HTMLElement = document.createElement("tr");
+  let tr = document.createElement("tr") as HTMLTableRowElement;
 
-  let td1: HTMLElement = document.createElement("td");
+  let td1 = document.createElement("td") as HTMLTableDataCellElement;
   td1.setAttribute("class", "text-center");
 
-  let i1: HTMLElement = document.createElement("i");
+  let i1 = document.createElement("i");
   i1.className = "fa fa-close";
   i1.addEventListener("click", (): void => {
     i1.parentElement.parentElement.remove();
@@ -27,10 +27,10 @@ function addDo(goal: string, date: string): void {
   td1.append(i1);
   tr.append(td1);
 
-  let td2: HTMLElement = document.createElement("td");
+  let td2 = <HTMLTableDataCellElement>document.createElement("td");
   td2.setAttribute("class", "text-center");
 
-  let i2: HTMLElement = document.createElement("i");
+  let i2 = document.createElement("i");
   i2.className = "fa fa-edit";
   i2.addEventListener("click", function (): void {
     let parent: HTMLElement = this.parentElement.parentElement;
@@ -48,20 +48,20 @@ function addDo(goal: string, date: string): void {
   td2.append(i2);
   tr.append(td2);
 
-  let td3: HTMLElement = document.createElement("td");
+  let td3 = <HTMLTableDataCellElement>document.createElement("td");
   td3.setAttribute("class", "text-center");
 
   td3.innerHTML = `${count}`;
 
   tr.append(td3);
 
-  let td4: HTMLElement = document.createElement("td");
+  let td4 = <HTMLTableDataCellElement>document.createElement("td");
   td4.setAttribute("class", "text-center goal");
 
   td4.innerHTML = goal;
   tr.append(td4);
 
-  let td5: HTMLElement = document.createElement("td");
+  let td5 = <HTMLTableDataCellElement>document.createElement("td");
   td5.setAttribute("class", "text-center date");
 
   td5.innerHTML = date;
